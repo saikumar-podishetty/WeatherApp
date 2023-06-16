6cf3ceb28290c6134996711e23278792732dfab3
sai kumar <sai.podishetty@dazn.com>
2023-06-16 17:05:19 +0530

    Created Project
    Created Project

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Sai Podishetty on 6/16/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
